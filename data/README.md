451d015616d21c32414e99f840df57970be4237d
Diego Lopez <diego@Diegos-MacBook-Pro-2.local>
2022-08-13 15:18:11 -0400

    Added file structures and preprocessing format

---
+++ data/README.md
@@ -0,0 +1,5 @@
+# Data 
+
+This folder contains the data for analysis. 
+
+All data can be downloaded from http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
---
+++ src/preprocessing.py
@@ -0,0 +1,27 @@
+# This file performs basic preprocessing on the KDD Cup data downloaded from the website: 
+# http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
+# TODO: One hot encoding / label encoding of categorical features
+
+import pandas as pd
+import numpy as np
+
+attack_arr = np.genfromtxt("../data/raw/training_attack_types.txt", dtype=str)
+attack_dict = {}
+for pair in attack_arr:
+    attack_dict[pair[0]] = pair[1]
+
+names = np.genfromtxt("../data/raw/kddcup.names.txt", dtype=str, encoding=None, skip_header=1)
+cols = np.append(names.transpose()[0], "labels")
+test_df = pd.read_csv("../data/raw/corrected")
+test_df.columns = cols
+train_10pct_df = pd.read_csv("../data/raw/kddcup.data_10_percent")
+train_10pct_df.columns = cols
+train_10pct_df = train_10pct_df.dropna()
+test_df = test_df.dropna()
+train_10pct_df['labels'] = train_10pct_df['labels'].str.replace(".", "")
+train_10pct_df['attack'] = train_10pct_df['labels'].replace(attack_dict)
+test_df['labels'] = test_df['labels'].str.replace(".", "")
+test_df['attack'] = test_df['labels'].replace(attack_dict)
+test_df.head(10)
+test_df.to_csv("../data/processed/test1.csv")
+train_10pct_df.to_csv("../data/processed/train1.csv")
